bf480ac76e31f65301755dddfba5a16d89435102
Pratik Vasagadekar <Vasagadekar.Pratik@gmail.com>
2024-01-01 22:16:09 +0530

    weather

--- app/internal/utils.py
+++ app/internal/utils.py
@@ -244,7 +244,9 @@ class parseRaidsData:
         weather_conditions = {7: "Fog", 6: "Snow", 5: "Windy", 4: "Cloudy", 3: "Partly Cloudy", 2: "Rainy", 1: "Clear"}
         self.dfRaids['weather'] = self.dfRaids['weather'].map(weather_conditions).fillna('')
         #* Add Boosted Condition
-        self.dfRaids['Boosted'] = self.dfRaids.apply(lambda row: "Boosted" if any(wb in row['weather'].split(', ') for wb in row['Weather Boost'].split(', ')) and row['weather'] else "Regular", axis=1)
+        self.dfRaids['Boosted'] = self.dfRaids.apply(
+    lambda row: "Boosted" if any(val in row['weather'] for val in row['Weather Boost'].split(',')) and row['weather'] != "" else "Regular",
+    axis=1)
 
         return self.dfRaids
           
